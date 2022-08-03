a7863ca39776ab47d5835deb79a3cc87bddde2b0
414ND1N <414nd1n@gmail.com>
2022-08-03 14:03:00 -0600

    Mejoras en el css (darkmode)

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -5,6 +5,7 @@
 	const menu_bar_pc = document.getElementById('menu_bar_pc')
 	const menu_bar_movil = document.getElementById('menu_bar_movil')
 	const section_contacto = document.getElementById('contacto')
+	const section_panaserver = document.getElementById('panaserver')
 
 	validarInput()
 
@@ -20,12 +21,14 @@
 			menu_bar_pc.className = 'menu-bar-pc darkmode'
 			menu_bar_movil.className = 'menu-bar-movil darkmode'
 			section_contacto.className = 'contacto darkmode'
+			section_panaserver.className = 'panaserver darkmode'
 		}else{
 			body.style.backgroundColor = 'white'
 			body.style.color = '#303133'
 			menu_bar_pc.className = 'menu-bar-pc'
 			menu_bar_movil.className = 'menu-bar-movil'
 			section_contacto.className = 'contacto'
+			section_panaserver.className = 'panaserver'
 		}
 	}
 
