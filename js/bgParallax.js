840a7d060a7fa19f1fdee1691a64925b569cb6b3
414ND1N <414nd1n@gmail.com>
2023-07-03 18:40:33 -0600

    Cambios CSS

--- js/bgParallax.js
+++ js/bgParallax.js
@@ -11,12 +11,12 @@
 			window.addEventListener('scroll', metParallax.scrollParallax);
 		},
 		scrollParallax : function(){
-			propParallax.recorrido = window.pageYOffset;
+			// obtener el recorrido del scroll
+			propParallax.recorrido = window.scrollY
+
 			propParallax.limite = propParallax.seccion.offsetTop + propParallax.seccion.offsetHeight
 			if (propParallax.recorrido > (propParallax.seccion.offsetTop- window.outerHeight) && propParallax.recorrido <=  propParallax.limite){
-
 				propParallax.seccion.style.backgroundPositionY = (propParallax.recorrido - propParallax.seccion.offsetTop) / 1.5 + 'px'
-
 			}else{
 				propParallax.seccion.style.backgroundPositionY = 0
 			}
