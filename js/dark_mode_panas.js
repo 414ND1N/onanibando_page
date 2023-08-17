8f27d20a7502d1dbd980bb002cb7a3fb546c0895
414ND1N <414nd1n@gmail.com>
2023-08-17 13:13:16 -0600

    Arreglo darkmode seccion panas

--- js/dark_mode_panas.js
+++ js/dark_mode_panas.js
@@ -4,7 +4,7 @@
 	const body = document.querySelector('body');
 	const menu_bar_pc = document.getElementById('menu_bar_pc')
 	const menu_bar_movil = document.getElementById('menu_bar_movil')
-	const section_container_tarjetas = document.getElementById('tarjetas_panas')
+	const section_container_tarjetas = document.getElementsByClassName('tarjetas')
 
 	// Verificar el estado almacenado en localStorage al cargar la página
     const darkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
@@ -23,14 +23,16 @@
 		if(darkModeEnabled){
 			menu_bar_pc.className = 'menu-bar-pc darkmode'
 			menu_bar_movil.className = 'menu-bar-movil darkmode'
-			section_container_tarjetas.className = 'tarjetas darkmode'
+			section_container_tarjetas[0].className = 'tarjetas darkmode'
+			section_container_tarjetas[1].className = 'tarjetas darkmode'
 			
 			body.style.backgroundColor = '#28292c'
 			body.style.color = 'white'
 		}else{
 			menu_bar_pc.className = 'menu-bar-pc'
 			menu_bar_movil.className = 'menu-bar-movil'
-			section_container_tarjetas.className = 'tarjetas'
+			section_container_tarjetas[0].className = 'tarjetas'
+			section_container_tarjetas[1].className = 'tarjetas'
 
 			body.style.backgroundColor = 'white'
 			body.style.color = '#303133'
